40178d3ee00b018ced38ec5c84fd5fb445b4b7c8
carlweilguny <weilguny.carl@gmx.at>
2020-07-13 21:36:47 +0200

    added CNN and SVM

--- data_split.py
+++ data_split.py
@@ -16,3 +16,14 @@ def prepare_data_spectrogram(path, flatten=False):
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, shuffle=False)
     X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, train_size=0.5, shuffle=False)
     return X_train, X_val, X_test, y_train, y_val, y_test
+
+def prepare_data_waveform(path):
+    file = h5py.File(path, 'r')
+    data = np.array(file['data'])
+    labels = np.array(file['labels'])
+    X = data.reshape((30000, 1, 8000))
+    y = labels
+    file.close()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, shuffle=False)
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, train_size=0.5, shuffle=False)
+    return X_train, X_val, X_test, y_train, y_val, y_test
